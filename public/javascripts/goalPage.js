dd345d923444a329a4cf47da802253838e1a7375
grneggandsam <samuel.louis.milligan@gmail.com>
2016-12-30 02:40:33 -0600

    Made the add Steps functionality work
    Also adjusted the data structure (API) and the CSS a bit

--- public/javascripts/goalPage.js
+++ public/javascripts/goalPage.js
@@ -2,8 +2,7 @@
 $(document).ready(function() {
 
     $('#btnAddGoal').on('click', addGoal);
-
-    //populateGoals();
+    $('#btnAddStep').on('click', addStep);
 
 });
 
@@ -53,7 +52,7 @@ function addGoal(event) {
         $.ajax({
             type: 'POST',
             data: newStep,
-            url: '/goals/addstepb',
+            url: '/goals/addgoal',
             dataType: 'JSON'
         }).done(function( response ) {
 
@@ -64,7 +63,59 @@ function addGoal(event) {
                 $('#addStep fieldset input').val('');
 
                 // Update page
-                alert("goal created");
+                location.reload();
+
+            }
+            else {
+                // If something goes wrong, alert the error message that our service returned
+                alert('Error: ' + response.msg);
+            }
+        });
+    }
+    else {
+        // If errorCount is more than 0, error out
+        alert('Please fill in all fields');
+        return false;
+    }
+};
+
+// Add Goal
+function addStep(event) {
+
+    $div = $(event.target).parent().parent("div");
+    id = $div.attr("id");
+    goal = $div.find("a").text();
+
+    event.preventDefault();
+
+    // Super basic validation - increase errorCount variable if any fields are blank
+    var errorCount = 0;
+    // $('#addGoal input').each(function(index, val) {
+    //     if($(this).val() === '') { errorCount++; }
+    // });
+
+    // Check and make sure errorCount's still at zero
+    if(errorCount === 0) {
+
+        var newStep = {
+            'goal': goal,
+            'step': $('#inputStepName').val(),
+            'description': $('#inputStepDesc').val(),
+            'date': $('#inputStepDate').val()
+        }
+
+        // Use AJAX to post the object to our adduser service
+        $.ajax({
+            type: 'POST',
+            data: newStep,
+            url: '/goals/addstepb',
+            dataType: 'JSON'
+        }).done(function( response ) {
+
+            // Check for successful (blank) response
+            if (response.msg === '') {
+
+                // Update page
                 location.reload();
 
             }
